8c4c0bdab95c938e3553bf1eca60347396c32a08
Vivian1407 <viviandutra41@gmail.com>
2024-09-11 17:10:52 -0300

    terceira linha duplicada

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Sistema de deteccao de doencas");
         System.out.println("Sistema de deteccao de doencas");
+        System.out.println("Sistema de deteccao de doencas");
     }
 }
