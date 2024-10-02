08d24ca9db6fae9d7f4aeb57a5943365a73432fe
Lia-mi <liviamicands@gmail.com>
2024-10-02 17:37:37 -0300

    alteracao do caminho das imagens

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("helloteste.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("layout_cadPac.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 300));
         primaryStage.show();
