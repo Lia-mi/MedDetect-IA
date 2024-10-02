cc328e44839b5b34cdbaf4a48028f04caee27ed8
LivyaT <livia.mtavares@outlook.com>
2024-10-02 16:46:05 -0300

    adicionando o bd

---
+++ src/bdMeddetect.sql
@@ -0,0 +1,22 @@
+create database meddetect;
+
+use meddetect;
+
+create table paciente
+(
+	paciente_id int primary key auto_increment,
+    paciente_nome varchar(128) not null,
+    paciente_nasc date not null,
+    paciente_email varchar(64) not null,
+    paciente_tel int not null
+);
+
+create table diagnostico
+(
+	diag_id int primary key auto_increment,
+	diag_imgURL varchar(128) not null,
+	diagnostico_IA text not null,
+	tipo varchar(80) not null,
+	paciente_id int,
+    foreign key(paciente_id) references paciente(paciente_id)
+);
