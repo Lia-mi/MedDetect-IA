1457389c58dba4f7f8be226d8f6995b0cef1bd12
Lia-mi <liviamicands@gmail.com>
2024-10-02 17:25:12 -0300

    Layout e Controller da pagina de cadastro do paciente

---
+++ src/CadPaController.java
@@ -0,0 +1,30 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class CadPaController {
+
+    @FXML
+    private Button botaoCadPaciente;
+
+    @FXML
+    private DatePicker data_Nasc;
+
+    @FXML
+    private PasswordField email_Paciente;
+
+    @FXML
+    private TextField nome_Paciente;
+
+    @FXML
+    private PasswordField telefone_Paciente;
+
+    @FXML
+    void cadastrarPaciente(ActionEvent event) {
+
+    }
+
+}
