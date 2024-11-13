9eba5cc9d041b90e4ba60c873f5d4edcea22bee7
Lia-mi <liviamicands@gmail.com>
2024-11-13 15:32:42 -0300

    Alteração controller cadastro paciente

--- src/CadPaController.java
+++ src/CadPaController.java
@@ -2,7 +2,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class CadPaController {
@@ -11,16 +10,13 @@ public class CadPaController {
     private Button botaoCadPaciente;
 
     @FXML
+    private TextField cpf_Paciente;
+
+    @FXML
     private DatePicker data_Nasc;
 
     @FXML
-    private PasswordField email_Paciente;
-
-    @FXML
     private TextField nome_Paciente;
-
-    @FXML
-    private PasswordField telefone_Paciente;
 
     @FXML
     void cadastrarPaciente(ActionEvent event) {
