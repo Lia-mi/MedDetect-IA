7e856e74c77ae68fd3b3f32599009ee16d8a87f7
luizinho2008 <luizhenriquemoreiracouto@gmail.com>
2024-12-04 17:36:12 -0300

    metodos e construtores do diagnosticos feitos

--- src/Diagnostico.java
+++ src/Diagnostico.java
@@ -1,3 +1,17 @@
 public class Diagnostico {
-    
+    private Item item;
+    private String imagem;
+
+    public Diagnostico(Item item, String imagem){
+        this.item = item;
+        this.imagem = imagem;
+    }
+
+    public Item getItem(){
+        return item;
+    }
+
+    public String getImagem(){
+        return imagem;
+    }
 }
