1389db44393d28e9d8bb51425f967ae3a6003fe1
Vivian1407 <viviandutra41@gmail.com>
2024-12-11 17:46:19 -0300

    Criando o controller do "gerarDiag"

---
+++ src/PleaseProvideControllerClassName.java
@@ -0,0 +1,57 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private Button MedIA;
+
+    @FXML
+    private Hyperlink artigos;
+
+    @FXML
+    private Button diagFeitos;
+
+    @FXML
+    private Label diagnostico;
+
+    @FXML
+    private Label doenca;
+
+    @FXML
+    private Label idade;
+
+    @FXML
+    private Label nome;
+
+    @FXML
+    private Button novoD;
+
+    @FXML
+    private TextField pesquisaDiag;
+
+    @FXML
+    private ImageView tomografia;
+
+    @FXML
+    void abrePesc(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mostraDiag(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voltarInicio(ActionEvent event) {
+
+    }
+
+}
+
