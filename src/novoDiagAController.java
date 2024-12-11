82dc26c5bb88eb743563fc5514cef0f9b944945a
Lia-mi <liviamicands@gmail.com>
2024-12-11 17:56:45 -0300

    implementação do file chooser junto de restrição para jpg e png

--- src/novoDiagAController.java
+++ src/novoDiagAController.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -34,10 +36,21 @@ public class novoDiagAController {
 
     }
 
+    @FXML
+    private ImageView imageView;
+    
     private Stage stage;
     @FXML
     void uploadImageC(ActionEvent event) {
         FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Escolha a sua tumografia");
+
+        FileChooser.ExtensionFilter jpgFilter = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.jpg");
+        FileChooser.ExtensionFilter pngFilter = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.png");
+
+        // Add os dois filtros pro file chooser
+        fileChooser.getExtensionFilters().addAll(jpgFilter, pngFilter);
+
         File selectedFile = fileChooser.showOpenDialog(stage);
     }
 
