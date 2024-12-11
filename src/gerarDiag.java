7b43865d4d7785a14132277ff30180baf3c13ece
Vivian1407 <viviandutra41@gmail.com>
2024-12-11 17:48:08 -0300

    Renomeando o controller

--- src/gerarDiag.java
+++ src/gerarDiag.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
-public class PleaseProvideControllerClassName {
+public class gerarDiag {
 
     @FXML
     private Button MedIA;
